fc8df8649e690d13e7866fd79204818b0bb9d524
Arthur <arthurvitorgn@gmail.com>
2022-12-20 23:39:20 -0300

    Commit

--- templates/static/js/script.js
+++ templates/static/js/script.js
@@ -18,98 +18,94 @@ const type_url = {
   water: 'https://images2.imgbox.com/d0/2c/JTRvjJR0_o.png'
 };
 
-// Get the name/id from the input field.
-function get_data(){
-  let element = document.getElementById('poke_name').value
-  makeRequest(element)
-}
 
-// Makes a request to the pokeapi using id or name.
-function makeRequest(id) {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    .then(response => response.json())
-    .then(data => replace_data([data['sprites'], data['types']]))
-    .catch(error => console.error());
-}
-
-// Insert the data into the card.
-function replace_data(data){
-  let type_space = document.getElementById('type_space')
-  type_space.innerHTML = ''
-
-  let sprites = data[0]
-  let types = data[1]
-
-  document.getElementById('normal_sprite_space').setAttribute('src', sprites['front_default'])
-  document.getElementById('shiny_sprite_space').setAttribute('src', sprites['front_shiny'])
-
-  types.forEach(tp => {
-    const img = document.createElement('img')
-    img.setAttribute('src', type_url[tp['type']['name']])
-    img.setAttribute('class', 'type_image')
-    type_space.appendChild(img)
-  });
-}
-
-let last_id = 4
-let array = [[],[],[]]
-
-function load_more() {
-  for (let index = last_id + 1; index <= last_id + 4; index++) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${index}`)
-      .then(response => response.json())
-      .then(data => {
-        array[0].push(data['name']);
-        array[1].push(data['sprites']);
-        array[2].push(data['types']);
-        if (array[0].length === 4) {
-          insert_pokemons(array);
-          last_id += 4;
-        }
-      })
-      .catch(error => console.error(error));
+async function getPokemonData(pokemonName) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+    if (response.status === 404) {
+      throw new Error(`Pokemon not found: ${pokemonName}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
   }
 }
 
+async function replace_data() {
+  let sprite = [];
+  let type = [];
+
+  // Requesting data from the pokeApi
+  let element = document.getElementById('poke_name').value;
+  let data = await getPokemonData(element);
+
+  // Assigning values/data
+  sprite = data['sprites']
+  types = data['types']
+
+  // Assigning normal sprite
+  let nr_sp = document.getElementById('normal_sprite_space')
+  nr_sp.setAttribute('src', sprite['front_default'])
+
+  // Assigning shiny sprite
+  let sh_sp = document.getElementById('shiny_sprite_space')
+  sh_sp.setAttribute('src', sprite['front_shiny'])
+
+  // Assigning the type(s)
+  let tp_sp = document.getElementById('type_space')
+  tp_sp.innerHTML = ''
+  try {
+    types.forEach(elm => { // Iterating through all the pokemon's type(s)
+      type = elm['type']
+
+      let img = document.createElement('img')  // Creating image element
+      img.src = type_url[type['name']] // Assigning the type url to the src
+      img.className = 'type_image'
+
+      tp_sp.appendChild(img) // Appending the img to the "type_space"
+
+    });
+  } catch (error) {
+    console.log(`A error ocourred: ${error}`)
+  }
+
+}
+
+let ls_pk_id = 4;
 let lst_id = 0
-function insert_pokemons(data){
-  let nm = data[0]
-  let spr = data[1]
-  let tp = data[2]
-  console.log();
-
-  lst_id ++
-  let pok_spc = document.getElementById('pokemons_space')
-
+async function load_more() {
+  lst_id += 1
+  let pk_sp = document.getElementById('pokemons_space')
 
   const div = document.createElement('div');
   div.className = 'card-container'
   div.id = 'cdc-'+lst_id
-  pok_spc.append(div)
+  pk_sp.append(div)
 
   let cdc_space = document.getElementById('cdc-'+lst_id)
 
-  for(let i = 0; i < 4; i++){
+  for (let c = ls_pk_id+1; c < ls_pk_id + 5; c++) {
+    let data = await getPokemonData(c)
+    let nm = data['name']
+    let spr = data['sprites']
+    let tp = data['types']
+
     const card = document.createElement('div')
-    card.className = `card ${tp[i][0]['type']['name']}`
+    card.className = `card ${tp[0]['type']['name']}`
     const a = document.createElement('a')
-    a.href = `/detailed_info:${nm[i]}`
+    a.href = `/detailed_info:${nm}`
     const img = document.createElement('img')
-    img.src = `${spr[i]['front_default']}`
+    img.src = `${spr['front_default']}`
     a.appendChild(img)
     card.appendChild(a)
     const p = document.createElement('p')
-    p.innerHTML = `${nm[i].charAt(0).toUpperCase() + nm[i].slice(1)}`
+    p.innerHTML = `${nm.charAt(0).toUpperCase() + nm.slice(1)}`
     card.appendChild(p)
     cdc_space.appendChild(card)
+
   }
 
-  array = [
-  [],
-  [],
-  []
-]
+  ls_pk_id += 4
 }
-
-
-
